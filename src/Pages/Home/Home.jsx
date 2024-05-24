3847579036f193980f1db430ae87a35adc36eabb
DevMousavi <mousavii.dev@gmail.com>
2024-05-24 13:53:13 +0330

    The Product Filtering Component Is Complete

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import Header from "../../LayOut/Header.jsx";
 import FilterBox from "../../Components/FilterBox/FilterBox.jsx";
 
@@ -11,6 +13,10 @@ const Home = () => {
             <main className={style.container_main}>
                 <div>
                     <FilterBox />
+                    <Link to="/" className={style.container_btn_more}>
+                        <p>مشاهده همه</p>
+                        <img src="./Icons/arrow-down.svg" alt="arrow.svg" />
+                    </Link>
                 </div>
             </main>
         </>
